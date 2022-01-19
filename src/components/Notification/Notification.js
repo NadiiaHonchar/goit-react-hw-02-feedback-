8672987874b437eeeb276f5b0241deeae75c42b4
NadiiaHonchar <17nmg82@gmail.com>
2022-01-19 16:39:22 +0200

    fixed

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,16 +1,16 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Notification = ({message}) => {
-    return(
-        <>
-        <p>{message}</p>        
-        </>
-    );
-} ;
+const Notification = ({ message }) => {
+  return (
+    <>
+      <p>{message}</p>
+    </>
+  );
+};
 
-Notification.propType={
-    message: PropTypes.string.isRequired,
+Notification.propType = {
+  message: PropTypes.string.isRequired,
 };
 
 export default Notification;
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import style from './Statistics.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import style from "./Statistics.module.css";
 
-const Statistics = ({good, neutral, bad, total, positivePercentage}) => {
-    return(        
-        <ul>
-            <li>Good: {good}</li>
-            <li>Neutral: {neutral}</li>
-            <li>Bad: {bad}</li>
-            <li>Total: {total}</li>
-            <li className={(positivePercentage < 50) ? style.alarm : style.normal}>Positive feedback: {positivePercentage}</li>
-        </ul>
-    )
-}
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <ul>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>Total: {total}</li>
+      <li className={positivePercentage < 50 ? style.alarm : style.normal}>
+        Positive feedback: {positivePercentage}
+      </li>
+    </ul>
+  );
+};
 
 Statistics.propTypes = {
-    good: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
-    positivePercentage: PropTypes.number.isRequired,
-}
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
 
 export default Statistics;
